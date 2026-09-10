fa44c52053c608b16231af960d86aefc8234c668
Lakshminiwash <lakshminiwash07rr@gmail.com>
2026-09-10 14:58:28 +0530

    restructured folder and added docker yml file

--- server.js
+++ server.js
@@ -1,27 +1,8 @@
 import express from "express"
+import app from "./src/app"
 
-const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
-app.get("/",(req,res)=>{
-    res.status(200).json({
-        message:"hello world !",
-    })
-})
-
-app.get("/app/data",(req,res)=>{
-    const data = {
-        id: 1234,
-        name:"hello world",
-        description:"this is description"
-    }
-    res.status(200).json(data)
-})
-
-app.get("/api/health", (req, res) => {
-    res.status(200).json({ status: "OK", timestamp: new Date() });
-})
-
-app.listen(PORT,()=>{
-    console.log(`server is running on port:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
 })
